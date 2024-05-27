4f748f4a502d01f698393759faa526311401ca74
SMARTSOFT\l.dudauri <lukadudauri01@gmail.com>
2024-05-27 16:15:05 +0400

    onReset

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,7 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResulModal(
-    { targetTime, remainingTime}, ref ) {
+    { targetTime, remainingTime, onReset}, ref ) {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
@@ -19,7 +19,7 @@ const ResultModal = forwardRef(function ResulModal(
         {userLost && <h2>You lost</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong></p>
-        <form method="dialog">
+        <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
     </dialog>
